f40736f861a768dd43a1b751779970f20c15f241
Denis Kulik <kulikdenis1996x@gmail.com>
2023-08-21 20:40:18 +0700

    add global styles

---
+++ src/GlobalStyles.ts
@@ -0,0 +1,65 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const theme: ThemeType = {
+    colors: {
+        primary: '#8856D9',
+        secondary: '#FF4136',
+        sea: '#A9C7C7',
+        white: '#FFF',
+        black: '#000',
+        grey: '#8C96A0',
+        darkgrey: '#30373F',
+        background: '#1C2023',
+    },
+    media: {
+        large: '(max-width: 1200px)',
+        medium: '(max-width: 900px)',
+        small: '(max-width: 600px)',
+    },
+}
+
+export const Global = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    scroll-behavior: smooth;
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+  }
+
+  html {
+    box-sizing: border-box;
+    font-family: Rubik, Roboto, sans-serif;
+    font-size: 62.5%; // 1 rem = 10px
+
+    @media ${props => props.theme.media.large} {
+      font-size: 56.25%; // 1 rem = 9px
+    }
+
+    @media ${props => props.theme.media.medium} {
+      font-size: 50%; // 1 rem = 8px
+    }
+  }
+`
+
+export type ThemeType = {
+    colors: {
+        primary: string
+        secondary: string
+        sea: string
+        white: string
+        black: string
+        grey: string
+        darkgrey: string
+        background: string
+    }
+    media: {
+        large: string
+        medium: string
+        small: string
+    }
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
+
+import { Global, theme } from 'GlobalStyles.ts'
 import { App } from 'App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <>
+    <ThemeProvider theme={theme}>
+        <Global />
         <App />
-    </>,
+    </ThemeProvider>,
 )
