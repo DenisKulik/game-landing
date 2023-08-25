acc6fa43ded2cfb574021ac0b8fe8dad3ebaf8ee
Denis Kulik <kulikdenis1996x@gmail.com>
2023-08-25 22:26:26 +0700

    fix: add container width & height

--- src/components/common/container/index.ts
+++ src/components/common/container/index.ts
@@ -2,6 +2,8 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
     max-width: 142rem;
+    width: 100%;
+    min-height: 100%;
     margin: 0 auto;
     padding: 0 1.5rem;
 `
