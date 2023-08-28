e82eebddcc3c0ec4743d7e1fd556a1803b95bb71
Denis Kulik <kulikdenis1996x@gmail.com>
2023-08-28 22:41:19 +0700

    feat: add the ability to watch the trailer

--- src/layout/sections/about/about.styles.ts
+++ src/layout/sections/about/about.styles.ts
@@ -1,8 +1,8 @@
 import styled, { css } from 'styled-components'
 import { SectionHeading } from 'components/common/section-heading'
 
-type Props = {
-    hidden?: boolean
+type VideoProps = {
+    playing?: string
 }
 
 export const StyledAbout = styled.section`
@@ -12,25 +12,27 @@ export const AboutHeading = styled(SectionHeading)`
     margin-bottom: 3.2rem;
 `
 
-export const VideoWrapper = styled.div`
+export const VideoWrapper = styled.div<VideoProps>`
     position: relative;
+
+    ${({ playing }) =>
+        playing &&
+        css`
+            ${VideoInfo} {
+                opacity: 0;
+                &:hover {
+                    opacity: 1;
+                }
+            }
+        `}
 `
 
-export const VideoInfo = styled.div<Props>`
+export const VideoInfo = styled.div`
     position: absolute;
     top: 50%;
     left: 50%;
     z-index: 2;
     transform: translate(-50%, -50%);
-
-    ${({ hidden }) =>
-        hidden &&
-        css`
-            opacity: 0;
-            &:hover {
-                opacity: 1;
-            }
-        `}
 
     & > button {
         margin: 0 auto;
--- src/layout/sections/about/index.tsx
+++ src/layout/sections/about/index.tsx
@@ -1,3 +1,4 @@
+import { useRef, useState } from 'react'
 import {
     AboutHeading,
     StyledAbout,
@@ -11,15 +12,31 @@ import trailer from 'assets/videos/trailer.mp4'
 import poster from 'assets/img/bg-video.webp'
 
 export const About = () => {
+    const [isVideoPlaying, setIsVideoPlaying] = useState(false)
+    const videoRef = useRef<HTMLVideoElement | null>(null)
+
+    const onPlayPauseVideo = () => {
+        const video = videoRef.current as HTMLVideoElement
+
+        if (video) {
+            isVideoPlaying ? video.pause() : video.play()
+            setIsVideoPlaying(!isVideoPlaying)
+        }
+    }
+
     return (
         <StyledAbout>
             <AboutHeading>About Game</AboutHeading>
-            <VideoWrapper>
+            <VideoWrapper playing={isVideoPlaying ? isVideoPlaying.toString() : ''}>
                 <VideoInfo>
                     <VideoTitle>Watch the Trailer</VideoTitle>
-                    <Button title="play" color="dark" />
+                    <Button
+                        onClick={onPlayPauseVideo}
+                        title={isVideoPlaying ? 'pause' : 'play'}
+                        color="dark"
+                    />
                 </VideoInfo>
-                <Video src={trailer} poster={poster}></Video>
+                <Video ref={videoRef} src={trailer} poster={poster}></Video>
             </VideoWrapper>
         </StyledAbout>
     )
